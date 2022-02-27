951692ad2f23101c2b55e4ab40dbf8ea9ca0e7bb
Rafael Aslanyan <rafaaslan923@gmail.com>
2022-02-27 18:18:18 +0300

    After urls and page tuning

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:my_slug>/', views.group_posts)
+    path('', views.index, name='main'),
+    path('group_list/', views.group_posts, name='group_posts')
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,9 +4,17 @@ from django.http import HttpResponse
 
 # Main page
 def index(request):
-    return HttpResponse(f'Главная страница')
+    template = 'posts/index.html'
+    context = {
+        'text': 'Это главная страница проекта Yatube'
+    }
+    return render(request, template, context)
 
 
 # Posts sorted by group
-def group_posts(request, my_slug):
-    return HttpResponse(f'Посты отфильтрованные по группам {my_slug}')
+def group_posts(request):
+    template = 'posts/group_list.html'
+    context = {
+        'text': 'Здесь будет информация о группах проекта Yatube'
+    }
+    return render(request, template, context)
